acfaeb51c6334c7eadc30a628b7051accc8b98ef
Ahmed <ahmedali.96@hotmail.com>
2020-07-27 16:28:14 -0400

    Updates

---
+++ precache-manifest.9176a64a6157d117b835cc80d7a8fd75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffbf299cde909789ac7635ab57cbae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df7940302571efa1cb11",
+    "url": "/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "5c2d042b7c9a33c8dabd",
+    "url": "/static/js/2.bd92cfa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd92cfa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7940302571efa1cb11",
+    "url": "/static/js/main.6e98c757.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
