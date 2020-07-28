b602635a95ab2b7e8bde83da19b13eefa029d85f
Ahmed <ahmedali.96@hotmail.com>
2020-07-28 13:44:56 -0400

    Updates

---
+++ precache-manifest.a39c0e8024c95b6a6ef350aa90b36dea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42901172f56bcd6eba7c071c1e2a8b78",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "69f63ab9ef35ba95a2de",
+    "url": "/portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "5c2d042b7c9a33c8dabd",
+    "url": "/portfolio/static/js/2.bd92cfa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.bd92cfa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f63ab9ef35ba95a2de",
+    "url": "/portfolio/static/js/main.632d366b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b244f0a55471c20f41c11287a6c4c2b4",
+    "url": "/portfolio/static/media/electron.b244f0a5.svg"
+  },
+  {
+    "revision": "6950f834a8442cd07d77ba21d87b84b3",
+    "url": "/portfolio/static/media/level up.6950f834.webp"
+  },
+  {
+    "revision": "038c82e7f612c1492a48d8ccf79c2438",
+    "url": "/portfolio/static/media/list.038c82e7.svg"
+  },
+  {
+    "revision": "2970de307c6aca0132f5315eb969edfa",
+    "url": "/portfolio/static/media/mic.2970de30.png"
+  },
+  {
+    "revision": "baf984154c4da5ff7319564b45f19f53",
+    "url": "/portfolio/static/media/pokeball.baf98415.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c99fc668bba2eb1d8cb5269a74c0bbc1.js"
+  "/portfolio/precache-manifest.a39c0e8024c95b6a6ef350aa90b36dea.js"
 );
 
 self.addEventListener('message', (event) => {
