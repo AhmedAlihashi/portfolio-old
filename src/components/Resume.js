7c9161a980ece545cd2de45ed04fd39678f5f0b7
Ahmed <ahmedali.96@hotmail.com>
2020-08-08 00:20:07 -0400

    alt props added

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -57,9 +57,18 @@ export default class Resume extends Component {
                 alt="github"
                 src="https://img.icons8.com/fluent/96/000000/github.png"
               />
-              <img src="https://img.icons8.com/color/96/000000/html-5.png" />
-              <img src="https://img.icons8.com/color/96/000000/css3.png" />
-              <img src="https://img.icons8.com/color/96/000000/javascript.png" />
+              <img
+                alt="html5"
+                src="https://img.icons8.com/color/96/000000/html-5.png"
+              />
+              <img
+                alt="css3"
+                src="https://img.icons8.com/color/96/000000/css3.png"
+              />
+              <img
+                alt="javascript"
+                src="https://img.icons8.com/color/96/000000/javascript.png"
+              />
               <p style={{ font: "inherit", fontSize: 12 }}>
                 Git, Github, Html, Css & Javascript
               </p>
@@ -67,13 +76,23 @@ export default class Resume extends Component {
 
             <div>
               <h5>Primary focuses</h5>
-              <img src="https://img.icons8.com/plasticine/100/000000/react.png" />
-              <img src="https://img.icons8.com/cute-clipart/100/000000/react-native.png" />
               <img
+                alt="react"
+                src="https://img.icons8.com/plasticine/100/000000/react.png"
+              />
+              <img
+                alt="react native"
+                src="https://img.icons8.com/cute-clipart/100/000000/react-native.png"
+              />
+              <img
+                alt="electron"
                 style={{ height: 80, margin: 10 }}
                 src="https://upload.wikimedia.org/wikipedia/commons/9/91/Electron_Software_Framework_Logo.svg"
               />
-              <img src="https://img.icons8.com/color/96/000000/redux.png" />
+              <img
+                alt="redux"
+                src="https://img.icons8.com/color/96/000000/redux.png"
+              />
               <p style={{ font: "inherit", fontSize: 12 }}>
                 React, React Native, Electron & redux
               </p>
@@ -82,10 +101,20 @@ export default class Resume extends Component {
             <div style={{ marginTop: 10 }}>
               <h5>backend & database tech</h5>
 
-              <img src="https://img.icons8.com/color/96/000000/nodejs.png" />
-              <img src="https://img.icons8.com/color/96/000000/mongodb.png" />
-              <img src="https://img.icons8.com/color/96/000000/firebase.png" />
               <img
+                alt="node.js"
+                src="https://img.icons8.com/color/96/000000/nodejs.png"
+              />
+              <img
+                alt="mongoDB"
+                src="https://img.icons8.com/color/96/000000/mongodb.png"
+              />
+              <img
+                alt="firebase"
+                src="https://img.icons8.com/color/96/000000/firebase.png"
+              />
+              <img
+                alt="express"
                 style={{ height: 80, margin: 5 }}
                 src="https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png"
               />
@@ -95,19 +124,40 @@ export default class Resume extends Component {
             </div>
             <div>
               <h5>Familiar workflow tech</h5>
-              <img src="https://img.icons8.com/color/96/000000/jira.png" />
-              <img src="https://img.icons8.com/color/96/000000/trello.png" />
-              <img src="https://img.icons8.com/windows/96/000000/sourcetree.png" />
-              <img src="https://img.icons8.com/officel/80/000000/slack.png" />
+              <img
+                alt="jira"
+                src="https://img.icons8.com/color/96/000000/jira.png"
+              />
+              <img
+                alt="trello"
+                src="https://img.icons8.com/color/96/000000/trello.png"
+              />
+              <img
+                alt="source tree"
+                src="https://img.icons8.com/windows/96/000000/sourcetree.png"
+              />
+              <img
+                alt="slack"
+                src="https://img.icons8.com/officel/80/000000/slack.png"
+              />
               <p style={{ font: "inherit", fontSize: 12 }}>
                 Jira, Trello, Sourcetree, Slack
               </p>
             </div>
             <div>
               <h5>Familiar design & prototyping software </h5>
-              <img src="https://img.icons8.com/color/96/000000/adobe-xd.png" />
-              <img src="https://img.icons8.com/color/96/000000/adobe-illustrator.png" />
-              <img src="https://img.icons8.com/windows/96/000000/invision.png" />
+              <img
+                alt="adobe xd"
+                src="https://img.icons8.com/color/96/000000/adobe-xd.png"
+              />
+              <img
+                alt="adobe illustrator"
+                src="https://img.icons8.com/color/96/000000/adobe-illustrator.png"
+              />
+              <img
+                alt="invision"
+                src="https://img.icons8.com/windows/96/000000/invision.png"
+              />
               <p style={{ font: "inherit", fontSize: 12 }}>
                 Adobe Xd, Illustrator, Invision
               </p>
